aaa213190c5ffaee6464457dd68e1e5543224792
reyhkm <reyhikam04@gmail.com>
2025-07-23 13:23:25 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,7 @@ function App() {
       <Projects onProjectClick={openModal} />
       <Skills />
       <Certifications />
-      <div className='relative z-0'>
-        <Contact />
-      </div>
+      <Contact />
       <Footer />
       <ChatWidget />
       <ProjectModal isOpen={isModalOpen} onClose={closeModal} project={selectedProject} />
